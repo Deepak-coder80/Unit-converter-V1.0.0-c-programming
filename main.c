4033b6710092c87e8a951ad0bcfa6a3320e2a04e
Deepak <souparnikad40@gmail.com>
2020-11-30 22:29:36 +0530

    Zero entry issue solved

--- main.c
+++ main.c
@@ -3,6 +3,7 @@
 Name        : Console Unit converter
 Author      : Deepak M S (GitHub:Deepak-coder80)
 CopyRight   : ISC-standard License
+version     : 1.0.0
 Description : Simply silly unit converter by c in console
 ======================================================================
 */
@@ -16,16 +17,18 @@ int main()
 {
     int choice;
     float value,result;
-    printf("***************\n");
-    printf("HAI WELCOME\n");
-    printf("***************\n");
+    printf("                        ***********************************************\n");
+    printf("                            HAI WELCOME TO THE UNIT CONVERTER\n ");
+    printf("                        ***********************************************\n");
     printf("\n");
     printf("Select your options form given below:\n");
-    printf(" 1.for convert kg to g \n 2. for convert g to kg\n" );
+    printf(" 1.For convert kg to g     3.For convert cm to m     5.For convert m/s to km/h\n 2.For convert g to kg     4.For convert m to cm     6.For convert km/h to m/s\n " );
     printf("Enter your choice here:");
     scanf("%d",&choice);
-    if(choice<=2){
-             printf("Enter the value you want to convert:");
+    if(choice==0){
+         printf("Invalid Entry, please check again!!!!");
+    }else if(choice<=6){
+        printf("Enter the value you want to convert:");
         scanf("%f",&value);
 
         switch(choice){
@@ -37,11 +40,27 @@ int main()
             result = value / 1000;
             printf("%f Kg",result);
             break;
+        case 3:
+            result = value / 100;
+            printf("%f m",result);
+            break;
+        case 4:
+            result = value * 100;
+            printf("%f cm",result);
+            break;
+        case 5:
+            result = value *3.6;
+            printf("%f km/h",result);
+            break;
+        case 6:
+            result = value / 3.6;
+            printf("%f m/s",result);
+            break;
 
         };
 
     }else{
-        printf("are you a fool??");
+        printf("Invalid Entry, please check again!!!!");
 
     };
 
